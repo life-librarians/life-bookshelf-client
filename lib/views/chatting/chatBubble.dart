fe44e0c4b5f744de32250af270c63e013e7be143
220v-K <jwlee010222@gmail.com>
2024-07-03 20:54:08 +0900

    feat/#2: design chat bubble

--- lib/views/chatting/chatBubble.dart
+++ lib/views/chatting/chatBubble.dart
@@ -10,7 +10,7 @@ class ChatBubble extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.all(8),
       child: CustomPaint(
-        painter: BubblePainter(),
+        painter: UserBubblePainter(),
         child: Container(
           padding: const EdgeInsets.only(left: 20, top: 15, right: 20, bottom: 15),
           child: Text(
@@ -23,37 +23,38 @@ class ChatBubble extends StatelessWidget {
   }
 }
 
-class BubblePainter extends CustomPainter {
+class UserBubblePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final Paint paint = Paint()
       ..color = const Color(0xFF6E8EFA)
       ..style = PaintingStyle.fill;
 
-    const double radius = 20;
+    const double radius = 12;
+    const double tailWidth = 30;
+    const double tailHeight = 15;
     final Path path = Path()
-      ..moveTo(radius, 0)
-      ..lineTo(size.width - radius, 0)
+      ..moveTo(radius, 0) // 좌상단 코너 시작
+      ..lineTo(size.width - radius, 0) // 상단 직선
       ..arcToPoint(
         Offset(size.width, radius),
         radius: const Radius.circular(radius),
-      )
-      ..lineTo(size.width, size.height - radius)
-      ..arcToPoint(
-        Offset(size.width - radius, size.height),
-        radius: const Radius.circular(radius),
-      )
-      ..lineTo(radius, size.height)
+      ) // 우상단 코너
+      ..lineTo(size.width, size.height) // 우측 직선
+      ..conicTo(size.width, size.height + tailHeight * 2, size.width - tailWidth, size.height, 1) // 첫 번째 곡선
+      // 3차 베지에 곡선을 통한 꼬리 구현
+      ..lineTo(size.width - tailWidth, size.height) // 우하단 몸통으로 복귀
+      ..lineTo(radius, size.height) // 좌하단 코너 시작지점으로 통하는 직선
       ..arcToPoint(
         Offset(0, size.height - radius),
         radius: const Radius.circular(radius),
-      )
-      ..lineTo(0, radius)
+      ) // 좌하단 코너
+      ..lineTo(0, radius) // 좌측 직선
       ..arcToPoint(
         const Offset(radius, 0),
         radius: const Radius.circular(radius),
-      )
-      ..close();
+      ) // 다시 좌상단 코너로
+      ..close(); // 경로 닫기
 
     canvas.drawPath(path, paint);
   }
--- lib/views/chatting/chatting_screen.dart
+++ lib/views/chatting/chatting_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:life_bookshelf/viewModels/chatting/chatting_viewmodel.dart';
 import 'package:life_bookshelf/views/base/base_screen.dart';
+import 'package:life_bookshelf/views/chatting/chatBubble.dart';
 
 class ChattingScreen extends BaseScreen<ChattingViewModel> {
   const ChattingScreen({super.key});
@@ -8,7 +9,13 @@ class ChattingScreen extends BaseScreen<ChattingViewModel> {
   @override
   Widget buildBody(BuildContext context) {
     return const Center(
-      child: Text("Chatting"),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Chatting"),
+          ChatBubble(message: "그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. 여름이었고, 엄마가 느티나무 아래에서 작은 파티를 준비해 주셨어. 그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. ..."),
+        ],
+      ),
     );
   }
 }
