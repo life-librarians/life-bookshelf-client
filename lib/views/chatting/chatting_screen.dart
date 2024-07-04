d26a20ae87d00cde3492f35dd829ff0326ed8c5b
220v-K <jwlee010222@gmail.com>
2024-07-04 17:57:56 +0900

    feat/#2: header 추가

--- lib/views/chatting/chatting_screen.dart
+++ lib/views/chatting/chatting_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:life_bookshelf/utilities/font_system.dart';
+import 'package:life_bookshelf/utilities/screen_utils.dart';
 import 'package:life_bookshelf/viewModels/chatting/chatting_viewmodel.dart';
 import 'package:life_bookshelf/views/base/base_screen.dart';
 import 'package:life_bookshelf/views/chatting/chatBubble.dart';
@@ -9,15 +12,84 @@ class ChattingScreen extends BaseScreen<ChattingViewModel> {
 
   @override
   Widget buildBody(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          ChatBubble(isUser: true, message: "그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. 여름이었고, 엄마가 느티나무 아래에서 작은 파티를 준비해 주셨어."),
-          ChatBubble(isUser: false, message: "그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. 여름이었고, 엄마가 느티나무 아래에서 작은 파티를 준비해 주셨어."),
-          ChatBubble(isUser: true, message: "어쩌라고"),
-          ChatBubble(isUser: false, message: ". . ."),
-          ChatBubble(isUser: true, message: ""),
+          chattingHeader(),
+          const ChatBubble(isUser: true, message: "그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. 여름이었고, 엄마가 느티나무 아래에서 작은 파티를 준비해 주셨어."),
+          const ChatBubble(isUser: false, message: "그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. 여름이었고, 엄마가 느티나무 아래에서 작은 파티를 준비해 주셨어."),
+          const ChatBubble(isUser: true, message: "어쩌라고"),
+          const ChatBubble(isUser: false, message: ". . ."),
+          const ChatBubble(isUser: true, message: ""),
+        ],
+      ),
+    );
+  }
+
+  Widget chattingHeader() {
+    return SizedBox(
+      height: 80.h,
+      child: Row(
+        children: [
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            child: const Icon(
+              Icons.chevron_left,
+              color: Colors.black,
+              size: 30,
+            ),
+            onPressed: () {
+              Get.back();
+            },
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Interview Chat", style: FontSystem.KR18SB),
+                Text("65% completed", style: FontSystem.KR10M.copyWith(color: Colors.black.withOpacity(0.5))),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5),
+                  child: ProgressBar(progress: 65),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(width: 30),
+        ],
+      ),
+    );
+  }
+}
+
+class ProgressBar extends StatelessWidget {
+  final double progress;
+
+  const ProgressBar({super.key, required this.progress});
+
+  final double barWidth = 220;
+  final double barHeight = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: barWidth,
+      height: barHeight,
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Stack(
+        children: [
+          Container(
+            width: barWidth * (progress / 100),
+            height: barHeight,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
         ],
       ),
     );
