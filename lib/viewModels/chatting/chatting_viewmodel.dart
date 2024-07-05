14e0b53e2bbcbfd88c4a99e813e284e79dd25a3b
220v-K <jwlee010222@gmail.com>
2024-07-06 02:40:49 +0900

    feat,setting/#10: 마이크 버튼 state 관리 및 마이크 권한 습득

--- lib/viewModels/chatting/chatting_viewmodel.dart
+++ lib/viewModels/chatting/chatting_viewmodel.dart
@@ -1,3 +1,29 @@
 import 'package:get/get.dart';
 
-class ChattingViewModel extends GetxController {}
+class ChattingViewModel extends GetxController {
+  final RxInt _micStateValue = 0.obs;
+
+  MicState get micState => MicState.fromInt(_micStateValue.value);
+
+  void changeMicState() {
+    _micStateValue.value = (_micStateValue.value + 1) % 3;
+    print("change mic state to $_micStateValue");
+  }
+}
+
+/// Mic Button's State
+/// 0 = ready (mic button)
+/// 1 = in progress (stop button)
+/// 2 = finish (check button)
+enum MicState {
+  ready(0),
+  inProgress(1),
+  finish(2);
+
+  final int value;
+  const MicState(this.value);
+
+  static MicState fromInt(int value) {
+    return MicState.values.firstWhere((e) => e.value == value % 3);
+  }
+}
--- lib/views/chatting/chatting_screen.dart
+++ lib/views/chatting/chatting_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:life_bookshelf/models/home/autobiography.dart';
+import 'package:life_bookshelf/utilities/color_system.dart';
 import 'package:life_bookshelf/utilities/font_system.dart';
 import 'package:life_bookshelf/utilities/screen_utils.dart';
 import 'package:life_bookshelf/viewModels/chatting/chatting_viewmodel.dart';
@@ -16,21 +20,43 @@ class ChattingScreen extends BaseScreen<ChattingViewModel> {
 
   @override
   Widget buildBody(BuildContext context) {
-    return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          chattingHeader(),
-          const ChatBubble(isUser: true, message: "그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. 여름이었고, 엄마가 느티나무 아래에서 작은 파티를 준비해 주셨어."),
-          const ChatBubble(isUser: false, message: "그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. 여름이었고, 엄마가 느티나무 아래에서 작은 파티를 준비해 주셨어."),
-          const ChatBubble(isUser: true, message: "어쩌라고"),
-          const ChatBubble(isUser: false, message: ". . ."),
-          const ChatBubble(isUser: true, message: ""),
-        ],
-      ).paddingSymmetric(horizontal: 15),
+    return Stack(
+      children: [
+        Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              chattingHeader(),
+              const Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ChatBubble(isUser: true, message: "그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. 여름이었고, 엄마가 느티나무 아래에서 작은 파티를 준비해 주셨어."),
+                      ChatBubble(isUser: false, message: "그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. 여름이었고, 엄마가 느티나무 아래에서 작은 파티를 준비해 주셨어."),
+                      ChatBubble(isUser: true, message: "그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. 여름이었고, 엄마가 느티나무 아래에서 작은 파티를 준비해 주셨어."),
+                      ChatBubble(isUser: false, message: "그 중에서도 가장 기억에 남는 순간은 내 생일 파티였어. 여름이었고, 엄마가 느티나무 아래에서 작은 파티를 준비해 주셨어."),
+                      ChatBubble(isUser: true, message: "어쩌라고"),
+                      ChatBubble(isUser: false, message: ". . ."),
+                      ChatBubble(isUser: false, message: ". . ."),
+                      ChatBubble(isUser: false, message: ". . ."),
+                      ChatBubble(isUser: true, message: ""),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ).paddingSymmetric(horizontal: 15),
+        ),
+        Obx(() => MicButton(
+              micState: viewModel.micState,
+              buttonFunction: viewModel.changeMicState,
+            )),
+      ],
     );
   }
 
+  /// Chatting Screen Header
   Widget chattingHeader() {
     return SizedBox(
       height: 80.h,
@@ -59,6 +85,71 @@ class ChattingScreen extends BaseScreen<ChattingViewModel> {
           ),
           const SizedBox(width: 30),
         ],
+      ),
+    );
+  }
+}
+
+class MicButton extends StatelessWidget {
+  final MicState micState;
+  final VoidCallback buttonFunction;
+
+  const MicButton({
+    super.key,
+    required this.micState,
+    required this.buttonFunction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    IconData buttonIcon;
+    Color buttonColor;
+
+    switch (micState) {
+      case MicState.ready:
+        buttonIcon = CupertinoIcons.mic_fill;
+        buttonColor = ColorSystem.mainBlue;
+        break;
+      case MicState.inProgress:
+        buttonIcon = CupertinoIcons.stop_fill;
+        buttonColor = CupertinoColors.systemRed;
+        break;
+      case MicState.finish:
+        buttonIcon = CupertinoIcons.checkmark_alt;
+        buttonColor = CupertinoColors.systemGreen;
+        break;
+    }
+
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 20, // 화면 하단에서의 거리
+      child: Center(
+        child: Container(
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+            color: buttonColor,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 0,
+                blurRadius: 10,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: CupertinoButton(
+            onPressed: buttonFunction,
+            padding: EdgeInsets.zero,
+            child: Icon(
+              buttonIcon,
+              color: ColorSystem.white,
+              size: (micState == MicState.finish) ? 55 : 40,
+            ),
+          ),
+        ),
       ),
     );
   }
