304d32ffa91dce799ca31a2137b52c4606da400e
bunju20 <ghkd2271@naver.com>
2024-10-12 18:24:44 +0900

    feat: 온보딩 질문 문항 추가

--- lib/models/onboarding/onboarding_model.dart
+++ lib/models/onboarding/onboarding_model.dart
@@ -4,12 +4,19 @@ class OnUserModel {
   String bornedAt;
   String gender;
   bool hasChildren;
+  String occupation;
+  String education_level;
+  String marital_status;
+
 
   OnUserModel({
     required this.name,
     required this.bornedAt,
     required this.gender,
     required this.hasChildren,
+    required this.occupation,
+    required this.education_level,
+    required this.marital_status,
   });
 
   // JSON 생성을 위한 메서드
@@ -19,6 +26,9 @@ class OnUserModel {
       'bornedAt': bornedAt,
       'gender': gender,
       'hasChildren': hasChildren,
+      'occupation': occupation,
+      'education_level': education_level,
+      'marital_status': marital_status,
     };
   }
 }
